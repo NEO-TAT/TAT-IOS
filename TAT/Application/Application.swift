300752a1d111a6357d9d62ed2d468cb804a0eb0d
jamfly <cocoredju222@gmail.com>
2019-08-24 14:28:50 +0800

    Init application

---
+++ TAT/Application/Application.swift
@@ -0,0 +1,15 @@
+//
+//  Application.swift
+//  TAT
+//
+//  Created by jamfly on 2019/8/24.
+//  Copyright © 2019 jamfly. All rights reserved.
+//
+
+import Foundation
+
+final class Application {
+  
+  static let shared = Application()
+  
+}
